84b3258e7bbc6352ca97d12724d76d5904533c42
Alexei Yashkov <alexei.yashkov@yashkov.org>
2023-08-26 14:13:09 -0400

    Switch to Jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverage: true,
+  coverageDirectory: "tmp/coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "text",
+    "lcov"
+  ]
+};
+
+module.exports = config;
