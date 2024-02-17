9b028a79a0faaf3806b50282f5f81d4347e6c401
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-02-17 12:14:23 -0500

    Adjust public API

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,10 +3,9 @@
  */
 
 export * from './lib/hal-error';
-export * from './lib/hal-base';
+export * from './lib/accessor';
 export * from './lib/resource';
 export * from './lib/collection';
-export * from './lib/accessor';
 export * from './lib/utils';
 export * from './lib/rxjs';
 export * from './lib/hal-client.service';
